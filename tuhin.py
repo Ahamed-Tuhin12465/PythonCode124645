9ec42a2ad63c1ff585ec323dc60843b4f047735c
Tuhin-Ahamed <76642850+Ahamed-Tuhin12465@users.noreply.github.com>
2021-02-13 16:24:49 +0600

    Add files via upload

---
+++ tuhin.py
@@ -0,0 +1,12 @@
+n = int(input('Enter The Number : '))
+sum = 0
+order = len(str(n))
+copy_n = n
+while(n>0):
+    digit = n%10
+    sum += digit **order
+    n = n//10
+if(sum == copy_n):
+    print(f"{copy_n} is an Amstrong Number")
+else:
+    print(f"{copy_n} is not an Amstong Number")
